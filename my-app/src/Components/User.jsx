74210082449757b945b336725360a4db13305f1d
Ravikantyadav18213 <raviyadav18213@gmail.com>
2025-11-28 11:30:46 +0000

    user2

---
+++ my-app/src/Components/User.jsx
@@ -0,0 +1,6 @@
+const users=[
+    {id:1,name:"Alice",email:"a@gmail.com",mobile:"62627"},
+    {id:2,name:"pooja",email:"p@gmail.com",mobile:"6677"},
+    {id:3,name:"rohan",email:"r@gmail.com",mobile:"62877"}
+]
+export default users;
