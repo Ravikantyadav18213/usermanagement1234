851efc9d7683f49479ce164153bce0d8985170a9
Ravikantyadav18213 <raviyadav18213@gmail.com>
2025-12-02 09:35:33 +0000

    register1

--- my-app/src/Components/Register.jsx
+++ my-app/src/Components/Register.jsx
@@ -1,63 +1,68 @@
 import { useState } from "react"
 
 export default function Register()
-{
-     const [formData.setforData]=useState({
-        uname:"",uemail:"",upass:"",ucpass:""
-     }) 
+ {
+    const [formData, setformData] = useState({
+        uname: "", uemail: "", upass: "", ucpass: ""
+    })
 
-     const [error,setError]=useState({});
-     const [successMsg,setsuccessMsg]=useState("");
+    const [errors, setError] = useState({});
+    const [successMsg, setsuccessMsg] = useState("");
 
-     const handlChange=(e)=>{
-        setforData({...formData,[e.target.value]:e.target.value})
-     }
+    const handleChange = (e) => {
+        setformData({ ...formData, [e.target.name]: e.target.value })
+    }
 
-     const validate=()=>
-        let newErrors={};
-     if(!formData.uname) newErrors.uname="Name is required";
-     if(!formData.uemail) newErrors.uname="Email is required";
-     if(!formData.upass) newErrors.uname="password is required";
-     if(!formData.ucpass) newErrors.uname="confirm password is required";
-     if(!formData.upass!==formData.ucpass) newErrors.uname="password nd confirm password dosent  is required";
-     return newErrors;
+    const validate = () =>{
+        let newErrors = {};
+    if (!formData.uname) newErrors.uname = "Name is required";
+    if (!formData.uemail) newErrors.uemail = "Email is required";
+    if (!formData.upass) newErrors.upass = "password is required";
+    if (!formData.ucpass) newErrors.ucpass = "confirm password is required";
+    if (formData.upass !== formData.ucpass) newErrors.ucpass = "password nd confirm password does not match";
+    return newErrors;
 
 }
-const handleSubmit=(e)=>{
+const handleSubmit = (e) => {
     e.preventsDefault();
-    const validateErrors=validate();
+    const validateErrors = validate();
     setError(validateErrors);
-    if(Object.keys(validateErrors).length===0)
-        {
-           setsuccessMsg("Registration done successfully");
+    if (Object.keys(validateErrors).length === 0) {
+        setsuccessMsg("Registration done successfully");
     }
-    else
-    {
+    else {
         setsuccessMsg("Registration failed")
     }
 }
-     
-    return(
-        <>
+
+return (
+    <>
         <h1>Register From</h1>
-        <form>
-         <label>Enter Name</label>
-         <input type="text" name="usernamr" />
-         <br/> <br />
-         <label>Enter Email</label>
-         <input type="text" name="uemail" />
-         <br/> <br />
-         <label>Enter password</label>
-         <input type="text" name="upass" />
-         <br/> <br />
-         <label>Enter comfirm password</label>
-         <input type="text" name="ucpass" />
-         <br/> <br />
-         
-         <input type="submit" name={"submit"} />
-         <input type="reset"name={"Cancle"} />
-         <br/> <br />
+        <form onSubmit={handleSubmit}>
+
+            {successMsg && <h2><mark>{successMsg}</mark></h2>}
+
+            <label>Enter Name</label>
+            <input type="text" name="uname" onChange={handleChange} />
+            {errors.uname && <p><mark>{errors.uname}</mark></p>}
+            <br /> <br />
+            <label>Enter Email</label>
+            <input type="text" name="uemail" onChange={handleChange} />
+            {errors.uemail && <p><mark>{errors.uemail}</mark></p>}
+            <br /> <br />
+            <label>Enter password</label>
+            <input type="text" name="upass" onChange={handleChange} />
+            {errors.upass && <p><mark>{errors.upass}</mark></p>}
+            <br /> <br />
+            <label>Enter comfirm password</label>
+            <input type="text" name="ucpass" onChange={handleChange} />
+            {errors.ucpass && <p><mark>{errors.ucpass}</mark></p>}
+            <br /> <br />
+
+            <input type="submit" name={"Submit"} />
+            <input type="reset" name={"Cancel"} />
+            <br /> <br />
         </form>
-        </>
-    )
-}
+    </>
+)
+ }
